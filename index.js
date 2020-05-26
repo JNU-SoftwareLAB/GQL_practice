0db4766470e7ac5ed615321600ac0cba45964d9c
forbid403 <forbid403@naver.com>
2020-05-26 21:41:32 +0900

    [add] main view

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-var query = `query { 
+const query = `query { 
     getBooks{
         title
+        author
     }
 }`;
 
-/*
-getBooks{ // 쿼리 명
-    title // 필드
-}
-*/
+function run() {
+    fetch('http://localhost:4000/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            query
+        })
+    }).then(r => r.json()).then(data => {
+        const list = document.querySelector('.booklists');
+        const books = data.data.getBooks;
 
-fetch('http://localhost:4000/graphql', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-        query
-    })
-}).then(r => r.json()).then(data => console.log('data returned:', data));
+        list.innerHTML = books.map((book) =>
+            `<div class='book'>
+            <h4><b>${book.title}</b></h4>
+            <p>${book.author}<p>
+        </div>`
+        ).join('');
+    });
+}
+
+
+function addBook() {
+
+    const mutation = `mutation{ 
+    addBook(id:30, author:"한강", title:"소년이 온다"){
+        id
+        author
+        title
+        description
+    }
+    }`;
+
+    fetch('http://localhost:4000/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            query: mutation
+        })
+    }).then(r => console.log(r))
+}
